ecc3587195d7673a34ff282527fcb387716a0ed8
McMomo <dev.sleepapi@gmail.com>
2025-01-07 22:17:27 +0100

    fix: divide of-each-ing by total nr of ings

--- frontend/src/components/calculator/results/member-results/member-production-skill-details/ingredient-magnet-s-details.vue
+++ frontend/src/components/calculator/results/member-results/member-production-skill-details/ingredient-magnet-s-details.vue
@@ -31,7 +31,9 @@
         <v-img src="/images/ingredient/ingredients.png" height="20" width="20"></v-img>
         <span class="font-weight-medium text-no-wrap text-center ml-2"> {{ totalSkillValue }} total</span>
       </div>
-      <span class="font-weight-light font-italic responsive-text text-center"> {{ totalSkillValue }} of each ing</span>
+      <span class="font-weight-light font-italic responsive-text text-center">
+        {{ amountOfEachIngredient }} of each ing</span
+      >
     </v-col>
   </v-row>
 </template>
@@ -41,7 +43,7 @@ import { StrengthService } from '@/services/strength/strength-service'
 import { mainskillImage } from '@/services/utils/image-utils'
 import { useTeamStore } from '@/stores/team/team-store'
 import type { MemberProductionExt } from '@/types/member/instanced'
-import { MathUtils, compactNumber } from 'sleepapi-common'
+import { MathUtils, compactNumber, ingredient } from 'sleepapi-common'
 import { defineComponent, type PropType } from 'vue'
 
 export default defineComponent({
@@ -67,6 +69,18 @@ export default defineComponent({
           amount: this.memberWithProduction.production.skillAmount,
           timeWindow: this.teamStore.timeWindow
         })
+      )
+    },
+    amountOfEachIngredient() {
+      return compactNumber(
+        MathUtils.round(
+          StrengthService.skillValue({
+            skill: this.memberWithProduction.member.pokemon.skill,
+            amount: this.memberWithProduction.production.skillAmount,
+            timeWindow: this.teamStore.timeWindow
+          }) / ingredient.TOTAL_NUMBER_OF_INGREDIENTS,
+          2
+        )
       )
     },
     timeWindowFactor() {
