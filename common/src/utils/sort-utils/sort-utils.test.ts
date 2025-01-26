64901bb9d1ad937b4d8f7f67b26ea1fe45096f15
jackylau-nyc <jackylau.email@gmail.com>
2025-01-26 09:55:52 +0800

    fix sort by pokedexNumber test

--- common/src/utils/sort-utils/sort-utils.test.ts
+++ common/src/utils/sort-utils/sort-utils.test.ts
@@ -19,17 +19,34 @@ import {
   VULPIX,
   VULPIX_ALOLAN
 } from '../../domain/pokemon';
-import { Pokemon } from '../../domain/pokemon/pokemon';
-import {
-  sortPokemonByField
-} from './sort-utils';
+import type { Pokemon } from '../../domain/pokemon/pokemon';
+import { sortPokemonByField } from './sort-utils';
 
 describe('sortPokemonByField', () => {
-  const pokemonList: Pokemon[] = [DEDENNE, DRAGONITE, FLAREON, GALLADE, GLACEON, LEAFEON, MAGNEZONE, VULPIX, VULPIX_ALOLAN, PIKACHU, PIKACHU_CHRISTMAS, RAICHU, RAIKOU, SYLVEON, TOGEKISS, ABSOL, CATERPIE, CHARIZARD];
+  const pokemonList: Pokemon[] = [
+    DEDENNE,
+    DRAGONITE,
+    FLAREON,
+    GALLADE,
+    GLACEON,
+    LEAFEON,
+    MAGNEZONE,
+    VULPIX,
+    VULPIX_ALOLAN,
+    PIKACHU,
+    PIKACHU_CHRISTMAS,
+    RAICHU,
+    RAIKOU,
+    SYLVEON,
+    TOGEKISS,
+    ABSOL,
+    CATERPIE,
+    CHARIZARD
+  ];
 
   it('should sort by name in ascending order', () => {
     const sorted = sortPokemonByField(pokemonList, 'name', 'asc');
-    expect(sorted.map(pokemon => pokemon.name)).toEqual([
+    expect(sorted.map((pokemon) => pokemon.name)).toEqual([
       'ABSOL',
       'CATERPIE',
       'CHARIZARD',
@@ -47,76 +64,46 @@ describe('sortPokemonByField', () => {
       'SYLVEON',
       'TOGEKISS',
       'VULPIX',
-      'VULPIX_ALOLAN']);
+      'VULPIX_ALOLAN'
+    ]);
   });
 
   it('should sort by name in descending order', () => {
     const sorted = sortPokemonByField(pokemonList, 'name', 'desc');
-    expect(sorted.map(pokemon => pokemon.name)).toEqual([    
-    'VULPIX_ALOLAN',
-    'VULPIX',
-    'TOGEKISS',
-    'SYLVEON',
-    'RAIKOU',
-    'RAICHU',
-    'PIKACHU_CHRISTMAS',
-    'PIKACHU',
-    'MAGNEZONE',
-    'LEAFEON',
-    'GLACEON',
-    'GALLADE',
-    'FLAREON',
-    'DRAGONITE',
-    'DEDENNE',
-    'CHARIZARD',
-    'CATERPIE',
-    'ABSOL']);
+    expect(sorted.map((pokemon) => pokemon.name)).toEqual([
+      'VULPIX_ALOLAN',
+      'VULPIX',
+      'TOGEKISS',
+      'SYLVEON',
+      'RAIKOU',
+      'RAICHU',
+      'PIKACHU_CHRISTMAS',
+      'PIKACHU',
+      'MAGNEZONE',
+      'LEAFEON',
+      'GLACEON',
+      'GALLADE',
+      'FLAREON',
+      'DRAGONITE',
+      'DEDENNE',
+      'CHARIZARD',
+      'CATERPIE',
+      'ABSOL'
+    ]);
   });
 
   it('should sort by pokedex number in ascending order', () => {
     const sorted = sortPokemonByField(pokemonList, 'pokedexNumber', 'asc');
-    expect(sorted.map(pokemon => pokemon.pokedexNumber)).toEqual([    
-    'CATERPIE',
-    'CHARIZARD',
-    'VULPIX_ALOLAN',
-    'VULPIX',
-    'PIKACHU_CHRISTMAS',
-    'PIKACHU',
-    'RAICHU',
-    'DRAGONITE',
-    'FLAREON',
-    'MAGNEZONE',
-    'LEAFEON',
-    'GLACEON',
-    'GALLADE',
-    'TOGEKISS',
-    'RAIKOU',
-    'ABSOL',
-    'SYLVEON',
-    'DEDENNE']);
+    expect(sorted.map((pokemon) => pokemon.pokedexNumber)).toEqual([
+      6, 10, 25, 25, 26, 37, 37, 136, 149, 243, 359, 462, 468, 470, 471, 475, 700, 702
+    ]);
   });
 
   it('should sort by pokedex number in descending order', () => {
     const sorted = sortPokemonByField(pokemonList, 'pokedexNumber', 'desc');
-    expect(sorted.map(pokemon => pokemon.pokedexNumber)).toEqual([    
-    'DEDENNE',
-    'SYLVEON',
-    'ABSOL',
-    'RAIKOU',
-    'TOGEKISS',
-    'GALLADE',
-    'GLACEON',
-    'LEAFEON',
-    'MAGNEZONE',
-    'FLAREON',
-    'DRAGONITE',
-    'RAICHU',
-    'PIKACHU',
-    'PIKACHU_CHRISTMAS',
-    'VULPIX',
-    'VULPIX_ALOLAN',
-    'CHARIZARD',
-    'CATERPIE']);
+    expect(sorted.map((pokemon) => pokemon.pokedexNumber)).toEqual([
+      702, 700, 475, 471, 470, 468, 462, 359, 243, 149, 136, 37, 37, 26, 25, 25, 10, 6
+    ]);
   });
 
   it('should handle an empty list', () => {
