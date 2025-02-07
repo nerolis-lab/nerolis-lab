a16c764e7f0f44b9fa1af0fc9dd44667f7e30744
tindomerel8 <tindomerel8@gmail.com>
2025-02-07 17:04:39 -0500

    feat: add footer with links

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -7,18 +7,21 @@
       <!-- <ServiceWorkerUpdate /> -->
       <RouterView />
     </v-main>
+    <TheFooter></TheFooter>
   </v-app>
 </template>
 
 <script lang="ts">
 import TheBackground from '@/components/background/background.vue'
 import TheNavbar from '@/components/nav-bar/nav-bar.vue'
+import TheFooter from '@/components/footer/footer.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   components: {
     TheNavbar,
-    TheBackground
+    TheBackground,
+    TheFooter
   }
 })
 </script>
---
+++ frontend/src/components/footer/footer.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-footer class="footer flex-center">
+    <div class="link-container">
+      <a href="https://feedback.nerolislab.com">
+        <v-icon class="icon" icon="mdi-forum"></v-icon>
+        <span>Feedback</span>
+      </a>
+      <a href="https://github.com/nerolis-lab/nerolis-lab" target="_blank" rel="noopener noreferrer">
+        <GithubIcon class="icon"></GithubIcon>
+        <span>GitHub</span>
+        <v-icon class="open-icon" icon="mdi-open-in-new" size="x-small"></v-icon>
+      </a>
+      <a href="https://discord.gg/ndzTXRHWzK" target="_blank" rel="noopener noreferrer">
+        <DiscordIcon class="icon"></DiscordIcon>
+        <span>Discord</span>
+        <v-icon class="open-icon" icon="mdi-open-in-new" size="x-small"></v-icon>
+      </a>
+    </div>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import DiscordIcon from '@/components/icons/icon-discord.vue'
+import GithubIcon from '@/components/icons/icon-github.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TheFooter',
+  components: { DiscordIcon, GithubIcon }
+})
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background: none;
+  border-top: 1px solid $secondary;
+  @media (min-width: 350px) {
+    max-height: 50px;
+    margin: 0 -7px;
+  }
+
+  @media (min-width: 400px) {
+    max-height: 50px;
+    margin: unset;
+  }
+
+  .link-container {
+    flex-grow: 1;
+    max-width: 800px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    flex-direction: column;
+
+    @media (min-width: 350px) {
+      flex-wrap: nowrap;
+      flex-direction: row;
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      min-height: 35px;
+
+      span {
+        text-decoration: underline;
+      }
+
+      .icon {
+        margin-right: 4px;
+      }
+
+      .open-icon {
+        margin-left: 2px;
+        @media (min-width: 400px) {
+          margin-left: 4px;
+        }
+      }
+    }
+  }
+}
+</style>
