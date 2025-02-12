0ccf72434d7cf4dc0ad1ce2f2eeac9e5e10c0993
jackylau-nyc <jackylau.email@gmail.com>
2025-02-12 09:24:52 +0800

    chore: add test for createIngredient function

--- common/src/domain/ingredient/ingredient.test.ts
+++ common/src/domain/ingredient/ingredient.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import type { Ingredient } from './ingredient';
 import * as IngredientsModule from './ingredients';
+import { createIngredient } from './ingredients';
 
 describe('INGREDIENTS array', () => {
   it('should include all dynamically defined ingredients in the INGREDIENTS array', () => {
@@ -19,5 +20,16 @@ describe('INGREDIENTS array', () => {
     allIngredients.forEach((ingredient) => {
       expect(IngredientsModule.INGREDIENTS).toContainEqual(ingredient);
     });
+  });
+});
+
+describe('createIngredient', () => {
+  it('should create an ingredient', () => {
+    const ingredient = createIngredient('Seaweed', 10, 15, 'Squirmy Seaweed');
+
+    expect(ingredient.name).toBe('Seaweed');
+    expect(ingredient.value).toBe(10);
+    expect(ingredient.taxedValue).toBe(15);
+    expect(ingredient.longName).toBe('Squirmy Seaweed');
   });
 });
