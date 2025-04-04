0bdd2f266d7c1fa4c86ef03e4a31b741f97345c5
McMomo <dev.sleepapi@gmail.com>
2025-04-04 21:43:07 +0200

    feat: adds support for dev database

--- backend/src/config/config.test.ts
+++ backend/src/config/config.test.ts
@@ -7,25 +7,27 @@ describe('config', () => {
     process.env = {};
 
     expect(backendConfig.config).toMatchInlineSnapshot(`
-{
-  "DATABASE_MIGRATION": undefined,
-  "DB_HOST": undefined,
-  "DB_PASS": undefined,
-  "DB_PORT": undefined,
-  "DB_USER": undefined,
-  "GENERATE_TIERLIST": false,
-  "GOOGLE_CLIENT_ID": undefined,
-  "GOOGLE_CLIENT_SECRET": undefined,
-  "NODE_ENV": "DEV",
-  "PORT": 3000,
-  "ROLLBACK_BATCHES": undefined,
-}
-`);
+      {
+        "DATABASE_MIGRATION": undefined,
+        "DATABASE_NAME": "pokemonsleep",
+        "DB_HOST": undefined,
+        "DB_PASS": undefined,
+        "DB_PORT": undefined,
+        "DB_USER": undefined,
+        "GENERATE_TIERLIST": false,
+        "GOOGLE_CLIENT_ID": undefined,
+        "GOOGLE_CLIENT_SECRET": undefined,
+        "NODE_ENV": "DEV",
+        "PORT": 3000,
+        "ROLLBACK_BATCHES": undefined,
+      }
+    `);
   });
 
   it('shall accept valid values', () => {
     const backendConfig = new BackendConfig();
     process.env.DATABASE_MIGRATION = 'UP';
+    process.env.DATABASE_NAME = 'some-database';
     process.env.DB_HOST = 'some-host';
     process.env.DB_PASS = 'some-pass';
     process.env.DB_PORT = '1';
@@ -38,20 +40,21 @@ describe('config', () => {
     process.env.GOOGLE_CLIENT_SECRET = 'some-google-secret';
 
     expect(backendConfig.config).toMatchInlineSnapshot(`
-{
-  "DATABASE_MIGRATION": "UP",
-  "DB_HOST": "some-host",
-  "DB_PASS": "some-pass",
-  "DB_PORT": "1",
-  "DB_USER": "some-user",
-  "GENERATE_TIERLIST": false,
-  "GOOGLE_CLIENT_ID": "some-google-id",
-  "GOOGLE_CLIENT_SECRET": "some-google-secret",
-  "NODE_ENV": "DEV",
-  "PORT": "2",
-  "ROLLBACK_BATCHES": 3,
-}
-`);
+      {
+        "DATABASE_MIGRATION": "UP",
+        "DATABASE_NAME": "some-database",
+        "DB_HOST": "some-host",
+        "DB_PASS": "some-pass",
+        "DB_PORT": "1",
+        "DB_USER": "some-user",
+        "GENERATE_TIERLIST": false,
+        "GOOGLE_CLIENT_ID": "some-google-id",
+        "GOOGLE_CLIENT_SECRET": "some-google-secret",
+        "NODE_ENV": "DEV",
+        "PORT": "2",
+        "ROLLBACK_BATCHES": 3,
+      }
+    `);
   });
 
   it('shall throw if DATABASE_MIGRATION is set to unexpected value', () => {
