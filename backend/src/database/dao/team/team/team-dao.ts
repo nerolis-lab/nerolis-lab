e48c1716a3160cd78248a6316b147ddde1e81684
3geek14 <nerd.of.pi@gmail.com>
2025-06-03 17:20:29 -0400

    feat(dao): sneaky snacking code
    Store sneaky snacking bool in TeamMemberDAO

--- backend/src/database/dao/team/team/team-dao.ts
+++ backend/src/database/dao/team/team/team-dao.ts
@@ -80,7 +80,7 @@ class TeamDAOImpl extends AbstractDAO<typeof DBTeamSchema> {
           skillLevel: member.skill_level,
           nature: member.nature,
           subskills,
-          sneakySnacking: false,
+          sneakySnacking: memberData.sneaky_snacking,
           ingredients: PokemonDAO.filterChosenIngredientList(member)
         });
       }
