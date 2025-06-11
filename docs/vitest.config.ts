1b1934831133d37f62f1056e3021333cadf80ef9
McMomo <dev.sleepapi@gmail.com>
2025-06-11 22:57:05 +0200

    build: split pipeline into multiple jobs, run docs

--- docs/vitest.config.ts
+++ docs/vitest.config.ts
@@ -7,11 +7,6 @@ export default defineConfig({
     include: ['tests/**/*.test.ts'],
     environment: 'node',
     testTimeout: 30000, // Longer timeout for network requests when checking links
-    root: fileURLToPath(new URL('./', import.meta.url)),
-    coverage: {
-      include: ['**/*.ts'],
-      exclude: ['**/node_modules/**', '**/test/**', '**/main.ts', '**/index.ts'],
-      reporter: ['text-summary', 'json', 'lcov']
-    }
+    root: fileURLToPath(new URL('./', import.meta.url))
   }
 });
