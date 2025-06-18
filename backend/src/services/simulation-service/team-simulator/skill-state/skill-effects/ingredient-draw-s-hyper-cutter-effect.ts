96957a9a394b77d3c1104f9a7e15aeef2cc1a409
Pi Fisher <Pi.L.D.Fisher@gmail.com>
2025-06-18 12:21:41 -0400

    feat(hyper cutter): use updated probabilities
    New data from RaenonX has somewhat more reliable estimates for the
    probabilities of each outcome for this skill. RaenonX is using
    different probabilities for each outcome, so I've rewritten the skill
    to support arbitrary probability distributions for the outcomes.
    
    The current distribution is based on a best-fit I ran with the
    following assumptions:
    
    * The ingredients don't need to have the same likelihoods as each
      other.
    * Each probability is a multiple of 1%.

--- backend/src/services/simulation-service/team-simulator/skill-state/skill-effects/ingredient-draw-s-hyper-cutter-effect.ts
+++ backend/src/services/simulation-service/team-simulator/skill-state/skill-effects/ingredient-draw-s-hyper-cutter-effect.ts
@@ -1,7 +1,6 @@
 import type { SkillEffect } from '@src/services/simulation-service/team-simulator/skill-state/skill-effect.js';
 import type { SkillActivation } from '@src/services/simulation-service/team-simulator/skill-state/skill-state-types.js';
 import type { SkillState } from '@src/services/simulation-service/team-simulator/skill-state/skill-state.js';
-import type { Ingredient } from 'sleepapi-common';
 import {
   emptyIngredientInventoryFloat,
   flatToIngredientSet,
@@ -15,26 +14,17 @@ export class IngredientDrawSHyperCutterEffect implements SkillEffect {
     const skill = IngredientDrawSHyperCutter;
 
     const roll = skillState.rng();
-    const rolledIngredient: Ingredient = skillState.rng.randomElement(
-      IngredientDrawSHyperCutter.ingredientDrawIngredients
-    );
+    const { amountFunc, critAmountFunc, ingredient } = skill.rollToResult(roll);
+    const amount = amountFunc(skillState.skillLevel);
+    const critAmount = critAmountFunc(skillState.skillLevel);
+    const nrOfIngredients = amount + critAmount;
 
     const ingredientDrawIngredients = emptyIngredientInventoryFloat();
-
-    let nonCritAmount = 0;
-    let critAmount = 0;
-    if (roll < skill.critChance) {
-      critAmount = skill.activations.ingredients.critAmount(skillState.skillLevel);
-      ingredientDrawIngredients[ING_ID_LOOKUP[rolledIngredient.name]] = critAmount;
-    } else {
-      nonCritAmount = skillState.skillAmount(skill.activations.ingredients);
-      ingredientDrawIngredients[ING_ID_LOOKUP[rolledIngredient.name]] = nonCritAmount;
-    }
+    ingredientDrawIngredients[ING_ID_LOOKUP[ingredient.name]] = nrOfIngredients;
 
     skillState.memberState.cookingState?.addIngredients(ingredientDrawIngredients);
 
-    const produce = skillState.memberState.skillProduce;
-    produce.ingredients = flatToIngredientSet(
+    skillState.memberState.skillProduce.ingredients = flatToIngredientSet(
       ingredientSetToFloatFlat(skillState.memberState.skillProduce.ingredients)._mutateCombine(
         ingredientDrawIngredients,
         (a, b) => a + b
@@ -47,7 +37,7 @@ export class IngredientDrawSHyperCutterEffect implements SkillEffect {
         {
           unit: 'ingredients',
           self: {
-            regular: nonCritAmount,
+            regular: amount,
             crit: critAmount
           }
         }
