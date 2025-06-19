cf6f3736b6eabd196612da771d8f0c8fd0453299
Pi Fisher <Pi.L.D.Fisher@gmail.com>
2025-06-19 17:06:55 -0400

    refactor(mainskill): Add util function for rolls
    The new implementations of the Ingredient Draw skills will support more general probabilities for each outcome. This util function will be used across both skills, which is why it's getting a new util file.

---
+++ common/src/utils/mainskill-utils/mainskill-utils.ts
@@ -0,0 +1,22 @@
+export function rollToOutput<TSkillOutput extends string | number | symbol>(
+  roll: number,
+  probabilities: Record<TSkillOutput, number>
+): TSkillOutput {
+  const allOutputs: TSkillOutput[] = Object.keys(probabilities) as TSkillOutput[];
+
+  let totalProbability = 0;
+  for (const output of allOutputs) {
+    totalProbability += probabilities[output];
+  }
+
+  let cumulativeProbability = 0;
+  for (const output of allOutputs) {
+    cumulativeProbability += probabilities[output];
+    if (roll * totalProbability < cumulativeProbability) {
+      return output;
+    }
+  }
+
+  // In case of rounding issues, return some default value.
+  return allOutputs[0];
+}
