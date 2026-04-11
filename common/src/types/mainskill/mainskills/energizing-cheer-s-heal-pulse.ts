3bb3d0f9de789ff3afb571a4c024ed03ff6b57ab
tindo <tindomerel8@gmail.com>
2026-04-11 15:41:22 -0400

    feat: add Latias

--- common/src/types/mainskill/mainskills/energizing-cheer-s-heal-pulse.ts
+++ common/src/types/mainskill/mainskills/energizing-cheer-s-heal-pulse.ts
@@ -6,7 +6,7 @@ import { EnergizingCheerS } from './energizing-cheer-s';
 export const EnergizingCheerSHealPulse = new (class extends ModifiedMainskill {
   baseSkill = EnergizingCheerS;
   modifierName = 'Heal Pulse';
-  RP = [880, 1251, 1726, 2383, 3290, 4546]; // temp values because I don't have access to RP spreadsheet
+  RP = [1600, 2300, 3180, 4417, 6113, 8462];
   energyAmounts = [6, 8, 10, 13, 17, 22];
   helpsAmounts = [1, 2, 2, 3, 4, 4];
   latiosHelpsAmounts = [1, 1, 2, 2, 2, 3]; // unused until Latios is added to the game
