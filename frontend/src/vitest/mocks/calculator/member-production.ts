7ebe1043c251cb4e559a87e0d8c3d7d12e5e24be
Pi Fisher <Pi.L.D.Fisher@gmail.com>
2026-09-01 23:21:22 -0400

    chore: remove unused skillAmount fields
    Now that nothing is using these fields, we no longer have to track them.

--- frontend/src/vitest/mocks/calculator/member-production.ts
+++ frontend/src/vitest/mocks/calculator/member-production.ts
@@ -82,7 +82,6 @@ export function createMockMemberProduction(
       averageHelps: 0,
       skillCrits: 0,
       maxFrequency: 0,
-      skillRegularValue: 0,
       totalHelps: 0,
       dayPeriod: {
         averageEnergy: 0,
@@ -98,7 +97,6 @@ export function createMockMemberProduction(
       nightHelps: 0,
       nightHelpsBeforeSS: 0,
       nightHelpsAfterSS: 0,
-      skillCritValue: 0,
       wastedEnergy: 0,
       morningProcs: 0,
       carrySize: 0,
