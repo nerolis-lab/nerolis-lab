97fecdf7b07d3f94377dafa3ae56d68f7e903c97
Matt Gleason <mjgleason@foundationdevices.com>
2026-09-03 16:27:37 -0400

    feat(calculator): added meal plan UI

--- common/src/vitest/mocks/team/mock-team-settings.ts
+++ common/src/vitest/mocks/team/mock-team-settings.ts
@@ -1,3 +1,4 @@
+import { defaultMealPlan } from '../../../types/team/meal-plan';
 import type { TeamSettings, TeamSettingsDto } from '../../../types/team/team';
 import { mockIngredientSetFloatIndexed } from '../ingredient/mock-ingredient-set';
 import { islandInstance } from '../island';
@@ -9,6 +10,8 @@ export function teamSettingsDto(attrs?: Partial<TeamSettingsDto>): TeamSettingsD
     bedtime: '21:30',
     wakeup: '06:00',
     stockpiledIngredients: [],
+    recipeType: 'curry',
+    mealPlan: defaultMealPlan(),
     island: islandInstance(),
     ...attrs
   };
@@ -23,6 +26,8 @@ export function teamSettings(attrs?: Partial<TeamSettings>): TeamSettings {
     stockpiledIngredients: mockIngredientSetFloatIndexed(),
     potSize: 15,
     island: islandInstance(),
+    recipeType: 'curry',
+    mealPlan: defaultMealPlan(),
     ...attrs
   };
 }
