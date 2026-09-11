1b76dbafb0e33abe4117c527eab061eb21d14a9f
Matt Gleason <mjgleason@foundationdevices.com>
2026-09-11 17:37:46 -0400

    feat(pokemon): add prototype Mewtwo and Psystrike

--- backend/src/services/simulation-service/team-simulator/skill-state/skill-state.ts
+++ backend/src/services/simulation-service/team-simulator/skill-state/skill-state.ts
@@ -97,6 +97,9 @@ import {
   ZeroAmount
 } from 'sleepapi-common';
 
+import { PsystrikeEffect } from './skill-effects/berry-zone/psystrike-effect.js';
+import { Psystrike } from 'sleepapi-common';
+
 export class SkillState {
   memberState: MemberState;
   skillEffects: Map<Mainskill, SkillEffect>;
@@ -119,6 +122,7 @@ export class SkillState {
     this.rng = rng;
 
     this.skillEffects = new Map<Mainskill, SkillEffect>([
+      [Psystrike, new PsystrikeEffect()],
       [BerryBurst, new BerryBurstEffect()],
       [BerryBurstDisguise, new BerryBurstDisguiseEffect()],
       [BerryBurstDracoMeteor, new BerryBurstDracoMeteorEffect()],
