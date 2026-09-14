SleepAPI/SleepAPI

fix(rotation): normalize IV comparisons to simulated rotation timing

backend/src/services/api-service/production/production-service.test.ts
@@ -18,7 +18,9 @@ import {
   PINSIR,
   subskill
 } from 'sleepapi-common';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
+import { mocks } from '@src/vitest/index.js';
 
 describe('calculatePokemonProduction', () => {
   it('should calculate production for PINSIR with given details', () => {
@@ -188,5 +190,108 @@ describe('calculateIv', () => {
     expect(result.variants[0]).toHaveProperty('produceTotal');
     expect(result.variants[0].produceTotal.ingredients).toBeDefined();
     expect(result.variants[0].produceTotal.berries).toBeDefined();
+  });
+});
+
+describe('scheduled IVs', () => {
+  it('replaces every occurrence of the selected Pokemon with each variant without mutating the schedule', () => {
+    const original = mocks.teamMemberExt({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const variant = { ...original, settings: { ...original.settings, externalId: 'variant' } };
+    const partner = { ...original, settings: { ...original.settings, externalId: 'partner' } };
+    const schedule = [
+      { slotIndex: 0, externalId: 'original', startTime: '06:00' },
+      { slotIndex: 0, externalId: 'partner', startTime: '12:00' },
+      { slotIndex: 0, externalId: 'original', startTime: '18:00' }
+    ];
+    const result = calculateIv(
+      {
+        settings: mocks.teamSettingsExt({ schedule }),
+        members: [partner],
+        variants: [variant],
+        replacedMemberId: 'original'
+      },
+      2
+    );
+    expect(result.variants[0].produceTotal.berries.reduce((sum, berry) => sum + berry.amount, 0)).toBeGreaterThan(0);
+    expect(schedule.map((shift) => shift.externalId)).toEqual(['original', 'partner', 'original']);
+  });
+
+  it('requires the original build for target-based comparisons', () => {
+    expect(() =>
+      calculateIv(
+        {
+          settings: mocks.teamSettingsExt({
+            schedule: [
+              { slotIndex: 0, externalId: 'original', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 }
+            ]
+          }),
+          members: [],
+          variants: [],
+          replacedMemberId: 'original'
+        },
+        1
+      )
+    ).toThrow('Target-based IV calculations require the original member');
+  });
+
+  it('returns reference production from the same working windows as an identical variant', () => {
+    const referenceMember = mocks.teamMemberExt({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const id = referenceMember.settings.externalId;
+    const variant = { ...referenceMember, settings: { ...referenceMember.settings, externalId: 'variant' } };
+    const partner = { ...referenceMember, settings: { ...referenceMember.settings, externalId: 'partner' } };
+    const result = calculateIv(
+      {
+        settings: mocks.teamSettingsExt({
+          includeCooking: true,
+          schedule: [
+            { slotIndex: 0, externalId: id, startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+            { slotIndex: 0, externalId: 'partner', startTime: '06:05', type: 'pot-size' }
+          ]
+        }),
+        members: [partner],
+        variants: [variant],
+        replacedMemberId: id,
+        referenceMember
+      },
+      8
+    );
+    expect(result.reference).toBeDefined();
+    expect(result.reference!.produceTotal).toEqual(result.variants[0].produceTotal);
+    expect(result.reference!.skillProcs).toEqual(result.variants[0].skillProcs);
+    expect(result.reference!.produceTotal.berries.reduce((sum, berry) => sum + berry.amount, 0)).toBeGreaterThan(0);
+  });
+
+  it('runs cooking during conditional IV simulations so meal bonuses can reset', () => {
+    const original = mocks.teamMemberExt({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const variant = { ...original, settings: { ...original.settings, externalId: 'variant' } };
+    const partner = { ...original, settings: { ...original.settings, externalId: 'partner' } };
+    const cook = vi.spyOn(CookingState.prototype, 'cook');
+    try {
+      calculateIv(
+        {
+          settings: mocks.teamSettingsExt({
+            includeCooking: true,
+            schedule: [
+              { slotIndex: 0, externalId: 'original', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+              { slotIndex: 0, externalId: 'partner', startTime: '06:05', type: 'pot-size' }
+            ]
+          }),
+          members: [partner],
+          variants: [variant],
+          replacedMemberId: 'original',
+          referenceMember: { ...original, settings: { ...original.settings, externalId: 'original' } }
+        },
+        1
+      );
+      expect(cook).toHaveBeenCalledTimes(6);
+    } finally {
+      cook.mockRestore();
+    }
   });
 });

backend/src/services/api-service/production/production-service.ts
@@ -1,8 +1,11 @@
 import type { ProductionStats } from '@src/domain/computed/production.js';
 import { setupAndRunProductionSimulation } from '@src/services/simulation-service/simulation-service.js';
 import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
-import type { UserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
-import { TeamSimulator } from '@src/services/simulation-service/team-simulator/team-simulator.js';
+import {
+  defaultUserRecipes,
+  type UserRecipes
+} from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
+import { TeamSimulator, type RotationTrace } from '@src/services/simulation-service/team-simulator/team-simulator.js';
 import { getIngredientSet } from '@src/utils/production-utils/production-utils.js';
 import type { PreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
@@ -174,22 +177,65 @@ export function calculateSimple(
 }
 
 export function calculateIv(
-  params: { settings: TeamSettingsExt; members: TeamMemberExt[]; variants: TeamMemberExt[] },
+  params: {
+    settings: TeamSettingsExt;
+    members: TeamMemberExt[];
+    variants: TeamMemberExt[];
+    replacedMemberId?: string;
+    referenceMember?: TeamMemberExt;
+    userRecipes?: UserRecipes;
+  },
   iterations = 1400
 ): CalculateIvResponse {
-  const { settings, members, variants } = params;
+  const { settings, members, variants, replacedMemberId, referenceMember, userRecipes } = params;
+  const conditional = settings.schedule?.some((shift) => shift.type && shift.type !== 'time');
+  if (conditional && (!referenceMember || referenceMember.settings.externalId !== replacedMemberId)) {
+    throw new Error('Target-based IV calculations require the original member');
+  }
 
   const rng: PreGeneratedRandom = createPreGeneratedRandom();
+  const createSimulator = (member: TeamMemberExt, reference = false) => {
+    const variantSettings = {
+      ...settings,
+      schedule: settings.schedule?.map((shift) =>
+        !reference && shift.externalId === replacedMemberId
+          ? { ...shift, externalId: member.settings.externalId }
+          : shift
+      )
+    };
+    // Independent streams keep the reference run independent of the number of variants.
+    const simulationRng = conditional ? createPreGeneratedRandom() : rng;
+    const cookingState = settings.includeCooking
+      ? new CookingState(variantSettings, userRecipes ?? defaultUserRecipes(), simulationRng)
+      : undefined;
+    return new TeamSimulator({
+      settings: variantSettings,
+      members: [member, ...members],
+      cookingState,
+      iterations,
+      rng: simulationRng
+    });
+  };
+
+  if (conditional && referenceMember) {
+    const reference = createSimulator(referenceMember, true);
+    const comparisons = variants.map((variant) => createSimulator(variant));
+    const trace: RotationTrace = new Map();
+    for (let i = 0; i < iterations; i++) {
+      reference.simulate({ record: trace });
+      for (const comparison of comparisons) comparison.simulate({ replay: trace });
+    }
+    return {
+      reference: reference.ivResults(referenceMember.settings.externalId),
+      variants: comparisons.map((comparison, index) => comparison.ivResults(variants[index].settings.externalId))
+    };
+  }
+
   const variantResults: MemberProductionBase[] = [];
   for (const variant of variants) {
-    const teamWithVariant = [variant, ...members];
-    const teamSimulator = new TeamSimulator({ settings, members: teamWithVariant, iterations, rng });
-
-    for (let i = 0; i < iterations; i++) {
-      teamSimulator.simulate();
-    }
-
-    variantResults.push(teamSimulator.ivResults(variant.settings.externalId));
+    const simulator = createSimulator(variant);
+    for (let i = 0; i < iterations; i++) simulator.simulate();
+    variantResults.push(simulator.ivResults(variant.settings.externalId));
   }
 
   return { variants: variantResults };

backend/src/services/simulation-service/team-simulator/team-simulator.test.ts
@@ -25,7 +25,7 @@ import {
   subskill
 } from 'sleepapi-common';
 import { vimic } from 'vimic';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 const mockPokemonWithIngredients: PokemonWithIngredients = {
   pokemon: commonMocks.mockPokemon({
@@ -779,5 +779,54 @@ describe('maybeActivateTeamSkill (energy)', () => {
     expect(simulator.memberStates[0].energy).toBe(50);
     expect(simulator.memberStates[1].energy).toBe(60);
     expect(simulator.memberStates[2].energy).toBe(40);
+  });
+});
+
+describe('conditional rotation replay', () => {
+  it('replays all conditional slots across days while retaining time-based switches', () => {
+    const members = ['a', 'b', 'c', 'd', 'e', 'f'].map((externalId) =>
+      mocks.teamMemberExt({
+        settings: { ...mockMembers[0].settings, externalId },
+        pokemonWithIngredients: mockPokemonWithIngredients
+      })
+    );
+    const settings = mocks.teamSettingsExt({
+      schedule: [
+        { slotIndex: 0, externalId: 'a', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+        { slotIndex: 0, externalId: 'b', startTime: '06:05', type: 'pot-size' },
+        { slotIndex: 1, externalId: 'c', startTime: '06:00', type: 'tasty-chance', tastyChanceTarget: 30 },
+        { slotIndex: 1, externalId: 'd', startTime: '06:05', type: 'tasty-chance' },
+        { slotIndex: 2, externalId: 'e', startTime: '06:00' },
+        { slotIndex: 2, externalId: 'f', startTime: '12:00' }
+      ]
+    });
+    const reference = new TeamSimulator({ settings, members, iterations: 3 }) as any;
+    const comparison = new TeamSimulator({ settings, members, iterations: 3 }) as any;
+    const referenceChanges = vi.spyOn(reference, 'setActiveMembers');
+    const comparisonChanges = vi.spyOn(comparison, 'setActiveMembers');
+    const condition = vi.spyOn(reference, 'conditionReached');
+    const comparisonCondition = vi.spyOn(comparison, 'conditionReached').mockImplementation(() => {
+      throw new Error('A comparison must not decide when to rotate');
+    });
+    const trace = new Map<number, Map<number, number>>();
+    for (let day = 0; day < 3; day++) {
+      const calls = [0, 0];
+      condition.mockImplementation((shift: any) => {
+        const tick = calls[shift.slotIndex]++;
+        if (day === 2) return false;
+        return tick >= 2 + shift.slotIndex && (day === 1 || tick < 4 + shift.slotIndex);
+      });
+      referenceChanges.mockClear();
+      comparisonChanges.mockClear();
+      reference.simulate({ record: trace });
+      if (day === 0) expect([...trace.keys()]).toEqual([10, 15, 20, 25]);
+      if (day === 1) expect([...trace.keys()]).toEqual([10, 15]);
+      if (day === 2) expect([...trace.keys()]).toEqual([0]);
+      comparison.simulate({ replay: trace });
+      const ids = (spy: any) => spy.mock.calls.map(([active]: any) => active.map((member: any) => member.id));
+      expect(ids(comparisonChanges)).toEqual(ids(referenceChanges));
+      expect(ids(referenceChanges).some((active: string[]) => active.includes('f'))).toBe(true);
+    }
+    expect(comparisonCondition).not.toHaveBeenCalled();
   });
 });

nerolis-lab/nerolis-lab

backend/src/services/api-service/production/production-service.test.ts
@@ -18,7 +18,9 @@ import {
   PINSIR,
   subskill
 } from 'sleepapi-common';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
+import { mocks } from '@src/vitest/index.js';
 
 describe('calculatePokemonProduction', () => {
   it('should calculate production for PINSIR with given details', () => {
@@ -188,5 +190,108 @@ describe('calculateIv', () => {
     expect(result.variants[0]).toHaveProperty('produceTotal');
     expect(result.variants[0].produceTotal.ingredients).toBeDefined();
     expect(result.variants[0].produceTotal.berries).toBeDefined();
+  });
+});
+
+describe('scheduled IVs', () => {
+  it('replaces every occurrence of the selected Pokemon with each variant without mutating the schedule', () => {
+    const original = mocks.teamMember({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const variant = { ...original, settings: { ...original.settings, externalId: 'variant' } };
+    const partner = { ...original, settings: { ...original.settings, externalId: 'partner' } };
+    const schedule = [
+      { slotIndex: 0, externalId: 'original', startTime: '06:00' },
+      { slotIndex: 0, externalId: 'partner', startTime: '12:00' },
+      { slotIndex: 0, externalId: 'original', startTime: '18:00' }
+    ];
+    const result = calculateIv(
+      {
+        settings: mocks.teamSettings({ schedule }),
+        members: [partner],
+        variants: [variant],
+        replacedMemberId: 'original'
+      },
+      2
+    );
+    expect(result.variants[0].produceTotal.berries.reduce((sum, berry) => sum + berry.amount, 0)).toBeGreaterThan(0);
+    expect(schedule.map((shift) => shift.externalId)).toEqual(['original', 'partner', 'original']);
+  });
+
+  it('requires the original build for target-based comparisons', () => {
+    expect(() =>
+      calculateIv(
+        {
+          settings: mocks.teamSettings({
+            schedule: [
+              { slotIndex: 0, externalId: 'original', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 }
+            ]
+          }),
+          members: [],
+          variants: [],
+          replacedMemberId: 'original'
+        },
+        1
+      )
+    ).toThrow('Target-based IV calculations require the original member');
+  });
+
+  it('returns reference production from the same working windows as an identical variant', () => {
+    const referenceMember = mocks.teamMember({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const id = referenceMember.settings.externalId;
+    const variant = { ...referenceMember, settings: { ...referenceMember.settings, externalId: 'variant' } };
+    const partner = { ...referenceMember, settings: { ...referenceMember.settings, externalId: 'partner' } };
+    const result = calculateIv(
+      {
+        settings: mocks.teamSettings({
+          includeCooking: true,
+          schedule: [
+            { slotIndex: 0, externalId: id, startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+            { slotIndex: 0, externalId: 'partner', startTime: '06:05', type: 'pot-size' }
+          ]
+        }),
+        members: [partner],
+        variants: [variant],
+        replacedMemberId: id,
+        referenceMember
+      },
+      8
+    );
+    expect(result.reference).toBeDefined();
+    expect(result.reference!.produceTotal).toEqual(result.variants[0].produceTotal);
+    expect(result.reference!.skillProcs).toEqual(result.variants[0].skillProcs);
+    expect(result.reference!.produceTotal.berries.reduce((sum, berry) => sum + berry.amount, 0)).toBeGreaterThan(0);
+  });
+
+  it('runs cooking during conditional IV simulations so meal bonuses can reset', () => {
+    const original = mocks.teamMember({
+      pokemonWithIngredients: { pokemon: CHARMANDER, ingredientList: [{ ingredient: ingredient.HONEY, amount: 2 }] }
+    });
+    const variant = { ...original, settings: { ...original.settings, externalId: 'variant' } };
+    const partner = { ...original, settings: { ...original.settings, externalId: 'partner' } };
+    const cook = vi.spyOn(CookingState.prototype, 'cook');
+    try {
+      calculateIv(
+        {
+          settings: mocks.teamSettings({
+            includeCooking: true,
+            schedule: [
+              { slotIndex: 0, externalId: 'original', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+              { slotIndex: 0, externalId: 'partner', startTime: '06:05', type: 'pot-size' }
+            ]
+          }),
+          members: [partner],
+          variants: [variant],
+          replacedMemberId: 'original',
+          referenceMember: { ...original, settings: { ...original.settings, externalId: 'original' } }
+        },
+        1
+      );
+      expect(cook).toHaveBeenCalledTimes(6);
+    } finally {
+      cook.mockRestore();
+    }
   });
 });

backend/src/services/api-service/production/production-service.ts
@@ -1,8 +1,11 @@
 import type { ProductionStats } from '@src/domain/computed/production.js';
 import { setupAndRunProductionSimulation } from '@src/services/simulation-service/simulation-service.js';
 import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
-import type { UserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
-import { TeamSimulator } from '@src/services/simulation-service/team-simulator/team-simulator.js';
+import {
+  defaultUserRecipes,
+  type UserRecipes
+} from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
+import { TeamSimulator, type RotationTrace } from '@src/services/simulation-service/team-simulator/team-simulator.js';
 import { getIngredientSet } from '@src/utils/production-utils/production-utils.js';
 import type { PreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
@@ -174,22 +177,65 @@ export function calculateSimple(
 }
 
 export function calculateIv(
-  params: { settings: TeamSettings; members: TeamMember[]; variants: TeamMember[] },
+  params: {
+    settings: TeamSettings;
+    members: TeamMember[];
+    variants: TeamMember[];
+    replacedMemberId?: string;
+    referenceMember?: TeamMember;
+    userRecipes?: UserRecipes;
+  },
   iterations = 1400
 ): CalculateIvResponse {
-  const { settings, members, variants } = params;
+  const { settings, members, variants, replacedMemberId, referenceMember, userRecipes } = params;
+  const conditional = settings.schedule?.some((shift) => shift.type && shift.type !== 'time');
+  if (conditional && (!referenceMember || referenceMember.settings.externalId !== replacedMemberId)) {
+    throw new Error('Target-based IV calculations require the original member');
+  }
 
   const rng: PreGeneratedRandom = createPreGeneratedRandom();
+  const createSimulator = (member: TeamMember, reference = false) => {
+    const variantSettings = {
+      ...settings,
+      schedule: settings.schedule?.map((shift) =>
+        !reference && shift.externalId === replacedMemberId
+          ? { ...shift, externalId: member.settings.externalId }
+          : shift
+      )
+    };
+    // Independent streams keep the reference run independent of the number of variants.
+    const simulationRng = conditional ? createPreGeneratedRandom() : rng;
+    const cookingState = settings.includeCooking
+      ? new CookingState(variantSettings, userRecipes ?? defaultUserRecipes(), simulationRng)
+      : undefined;
+    return new TeamSimulator({
+      settings: variantSettings,
+      members: [member, ...members],
+      cookingState,
+      iterations,
+      rng: simulationRng
+    });
+  };
+
+  if (conditional && referenceMember) {
+    const reference = createSimulator(referenceMember, true);
+    const comparisons = variants.map((variant) => createSimulator(variant));
+    const trace: RotationTrace = new Map();
+    for (let i = 0; i < iterations; i++) {
+      reference.simulate({ record: trace });
+      for (const comparison of comparisons) comparison.simulate({ replay: trace });
+    }
+    return {
+      reference: reference.ivResults(referenceMember.settings.externalId),
+      variants: comparisons.map((comparison, index) => comparison.ivResults(variants[index].settings.externalId))
+    };
+  }
+
   const variantResults: MemberProductionBase[] = [];
   for (const variant of variants) {
-    const teamWithVariant = [variant, ...members];
-    const teamSimulator = new TeamSimulator({ settings, members: teamWithVariant, iterations, rng });
-
-    for (let i = 0; i < iterations; i++) {
-      teamSimulator.simulate();
-    }
-
-    variantResults.push(teamSimulator.ivResults(variant.settings.externalId));
+    const simulator = createSimulator(variant);
+    for (let i = 0; i < iterations; i++) simulator.simulate();
+    variantResults.push(simulator.ivResults(variant.settings.externalId));
   }
 
   return { variants: variantResults };

backend/src/services/simulation-service/team-simulator/team-simulator.test.ts
@@ -25,7 +25,7 @@ import {
   subskill
 } from 'sleepapi-common';
 import { vimic } from 'vimic';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 const mockPokemonWithIngredients: PokemonWithIngredients = {
   pokemon: commonMocks.mockPokemon({
@@ -772,5 +772,54 @@ describe('maybeActivateTeamSkill (energy)', () => {
     expect(simulator.memberStates[0].energy).toBe(50);
     expect(simulator.memberStates[1].energy).toBe(60);
     expect(simulator.memberStates[2].energy).toBe(40);
+  });
+});
+
+describe('conditional rotation replay', () => {
+  it('replays all conditional slots across days while retaining time-based switches', () => {
+    const members = ['a', 'b', 'c', 'd', 'e', 'f'].map((externalId) =>
+      mocks.teamMember({
+        settings: { ...mockMembers[0].settings, externalId },
+        pokemonWithIngredients: mockPokemonWithIngredients
+      })
+    );
+    const settings = mocks.teamSettings({
+      schedule: [
+        { slotIndex: 0, externalId: 'a', startTime: '06:00', type: 'pot-size', potSizeTarget: 200 },
+        { slotIndex: 0, externalId: 'b', startTime: '06:05', type: 'pot-size' },
+        { slotIndex: 1, externalId: 'c', startTime: '06:00', type: 'tasty-chance', tastyChanceTarget: 30 },
+        { slotIndex: 1, externalId: 'd', startTime: '06:05', type: 'tasty-chance' },
+        { slotIndex: 2, externalId: 'e', startTime: '06:00' },
+        { slotIndex: 2, externalId: 'f', startTime: '12:00' }
+      ]
+    });
+    const reference = new TeamSimulator({ settings, members, iterations: 3 }) as any;
+    const comparison = new TeamSimulator({ settings, members, iterations: 3 }) as any;
+    const referenceChanges = vi.spyOn(reference, 'setActiveMembers');
+    const comparisonChanges = vi.spyOn(comparison, 'setActiveMembers');
+    const condition = vi.spyOn(reference, 'conditionReached');
+    const comparisonCondition = vi.spyOn(comparison, 'conditionReached').mockImplementation(() => {
+      throw new Error('A comparison must not decide when to rotate');
+    });
+    const trace = new Map<number, Map<number, number>>();
+    for (let day = 0; day < 3; day++) {
+      const calls = [0, 0];
+      condition.mockImplementation((shift: any) => {
+        const tick = calls[shift.slotIndex]++;
+        if (day === 2) return false;
+        return tick >= 2 + shift.slotIndex && (day === 1 || tick < 4 + shift.slotIndex);
+      });
+      referenceChanges.mockClear();
+      comparisonChanges.mockClear();
+      reference.simulate({ record: trace });
+      if (day === 0) expect([...trace.keys()]).toEqual([10, 15, 20, 25]);
+      if (day === 1) expect([...trace.keys()]).toEqual([10, 15]);
+      if (day === 2) expect([...trace.keys()]).toEqual([0]);
+      comparison.simulate({ replay: trace });
+      const ids = (spy: any) => spy.mock.calls.map(([active]: any) => active.map((member: any) => member.id));
+      expect(ids(comparisonChanges)).toEqual(ids(referenceChanges));
+      expect(ids(referenceChanges).some((active: string[]) => active.includes('f'))).toBe(true);
+    }
+    expect(comparisonCondition).not.toHaveBeenCalled();
   });
 });