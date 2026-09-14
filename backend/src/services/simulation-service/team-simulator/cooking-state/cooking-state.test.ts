SleepAPI/SleepAPI

feat(calculator): meal plan logic

backend/src/services/simulation-service/team-simulator/cooking-state/cooking-state.test.ts
@@ -1,37 +1,46 @@
 import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
 import { defaultUserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
 import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
+import type { PreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { mocks } from '@src/vitest/index.js';
-import type { MealTimes } from 'sleepapi-common';
 import {
   dessert,
   emptyIngredientInventoryFloat,
   ingredient,
   ingredientSetToFloatFlat,
   MAX_POT_SIZE,
-  parseTime
+  parseTime,
+  recipeLevelBonus
 } from 'sleepapi-common';
 import { describe, expect, it } from 'vitest';
 
+function noCritRandom(): PreGeneratedRandom {
+  const rng = (() => 0.99) as PreGeneratedRandom;
+  rng.getUint8 = () => 255;
+  rng.getIndex = () => 0;
+  rng.randomElement = <T>(array: T[]) => array[0];
+  return rng;
+}
+
 describe('CookingState', () => {
-  it('shall include provided meal times in results', () => {
+  it('shall include average cooked meal times in results', () => {
     const cookingState = new CookingState(
       mocks.teamSettingsExt({ camp: true }),
       defaultUserRecipes(),
       createPreGeneratedRandom()
     );
 
-    const mealTimes: MealTimes = {
-      breakfast: parseTime('08:00'),
-      lunch: parseTime('14:00'),
-      dinner: parseTime('20:00')
-    };
-
-    cookingState.setMealTimes(mealTimes);
+    cookingState.recordMealCookTime('breakfast', 120);
+    cookingState.recordMealCookTime('lunch', 480);
+    cookingState.recordMealCookTime('dinner', 840);
 
     const result = cookingState.results(1);
 
-    expect(result.mealTimes).toEqual(mealTimes);
+    expect(result.mealTimes).toEqual({
+      breakfast: parseTime('08:00'),
+      lunch: parseTime('14:00'),
+      dinner: parseTime('20:00')
+    });
   });
 
   it('shall cook the best recipe for which it has ingredients', () => {
@@ -173,5 +182,77 @@ describe('CookingState', () => {
     cookingState.startNewWeek();
 
     expect(cookingState['currentDessertStockpile']).toEqual(initialStockpile);
+  });
+
+  it('shall cook a planned recipe early and add only unreserved filler value after recipe strength', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettingsExt({
+        potSize: 20,
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'recipe', recipe: dessert.WARM_MOOMOO_MILK.name },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addIngredients(
+      ingredientSetToFloatFlat([
+        ...dessert.WARM_MOOMOO_MILK.ingredients,
+        { amount: 5, ingredient: ingredient.SLOWPOKE_TAIL }
+      ])
+    );
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: false, sunday: false })).toBe(true);
+
+    const result = cookingState.results(1);
+    expect(result.dessert.weeklyStrength).toBe(
+      dessert.WARM_MOOMOO_MILK.value * recipeLevelBonus[60] + 5 * ingredient.SLOWPOKE_TAIL.value
+    );
+    expect(result.dessert.cookedRecipes[0].averageFillerValue).toBe(5 * ingredient.SLOWPOKE_TAIL.value);
+    expect(result.dessert.cookedRecipes[0].isPlannedRecipe).toBe(true);
+  });
+
+  it('shall skip None meals without consuming a pot-size bonus', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettingsExt({
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'none' },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addPotSize(10);
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: true, sunday: false })).toBe(false);
+    expect(cookingState['bonusPotSize']).toBe(10);
+  });
+
+  it('shall exclude ingredients reserved for planned recipes from deadline fallback recipes', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettingsExt({
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'recipe', recipe: dessert.CRAFT_SODA_POP.name },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addIngredients(ingredientSetToFloatFlat(dessert.FANCY_APPLE_JUICE.ingredients));
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: true, sunday: false })).toBe(true);
+    const cookedRecipe = cookingState.results(1).dessert.cookedRecipes[0];
+    expect(cookedRecipe.recipe.name).toBe(dessert.FANCY_APPLE_JUICE.name);
+    expect(cookedRecipe.averageFillerValue).toBe(0);
+    expect(cookedRecipe.isPlannedRecipe).toBe(false);
   });
 });

backend/src/services/simulation-service/team-simulator/team-simulator-utils.ts
@@ -1,8 +1,8 @@
 import { calculateHelpSpeedBeforeEnergy } from '@src/services/calculator/help/help-calculator.js';
 import type { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
-import { getDefaultMealTimes } from '@src/utils/meal-utils/meal-utils.js';
+import { getDefaultMealTimes, getMealWindows } from '@src/utils/meal-utils/meal-utils.js';
 import { TimeUtils } from '@src/utils/time-utils/time-utils.js';
-import type { FunctionalEvent, ProduceFlat, TeamMemberExt, TeamSettingsExt, TimePeriod } from 'sleepapi-common';
+import type { FunctionalEvent, MealSlot, ProduceFlat, TeamMemberExt, TeamSettingsExt, TimePeriod } from 'sleepapi-common';
 import {
   berrySetToFlat,
   calculateAveragePokemonIngredientSet,
@@ -15,6 +15,7 @@ class TeamSimulatorUtilsImpl {
   public setupSimulationTimes(params: { settings: TeamSettingsExt; cookingState?: CookingState }): {
     nightStartMinutes: number;
     mealTimeMinutesSinceStart: number[];
+    mealWindowsMinutesSinceStart: { meal: MealSlot; start: number; end: number }[];
   } {
     const { settings, cookingState } = params;
 
@@ -26,6 +27,7 @@ class TeamSimulatorUtilsImpl {
     const nightStartMinutes = TimeUtils.timeToMinutesSinceStart(settings.bedtime, settings.wakeup);
 
     const mealTimes = getDefaultMealTimes(dayPeriod);
+    const mealWindows = getMealWindows(dayPeriod);
     cookingState?.setMealTimes(mealTimes.meals);
     const mealTimeMinutesSinceStart = mealTimes.sorted.map((time) =>
       TimeUtils.timeToMinutesSinceStart(time, dayPeriod.start)
@@ -33,7 +35,12 @@ class TeamSimulatorUtilsImpl {
 
     return {
       nightStartMinutes,
-      mealTimeMinutesSinceStart
+      mealTimeMinutesSinceStart,
+      mealWindowsMinutesSinceStart: mealWindows.map(({ meal, start, end }) => ({
+        meal,
+        start: TimeUtils.timeToMinutesSinceStart(start, dayPeriod.start),
+        end: TimeUtils.timeToMinutesSinceStart(end, dayPeriod.start)
+      }))
     };
   }
 

backend/src/services/simulation-service/team-simulator/team-simulator.test.ts
@@ -4,7 +4,10 @@ import type {
   TeamActivationValue,
   UnitActivation
 } from '@src/services/simulation-service/team-simulator/skill-state/skill-state-types.js';
+import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
+import { defaultUserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
 import { TeamSimulator } from '@src/services/simulation-service/team-simulator/team-simulator.js';
+import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { mocks } from '@src/vitest/index.js';
 import type { Berry, PokemonSpecialty, PokemonWithIngredients, TeamMemberExt, TeamSettingsExt } from 'sleepapi-common';
 import {
@@ -63,6 +66,23 @@ const mockMembers: TeamMemberExt[] = [
 ];
 
 describe('TeamSimulator', () => {
+  it('shall cook a Best Recipe meal on the final simulation tick before bedtime', () => {
+    const settings = mocks.teamSettingsExt({
+      bedtime: parseTime('23:59'),
+      mealPlan: {
+        breakfast: { kind: 'none' },
+        lunch: { kind: 'none' },
+        dinner: { kind: 'best' }
+      }
+    });
+    const cookingState = new CookingState(settings, defaultUserRecipes(), createPreGeneratedRandom());
+    const simulator = new TeamSimulator({ settings, members: [], cookingState, iterations: 1 });
+
+    simulator.simulate();
+
+    expect(simulator.results().cooking?.mealTimes.dinner).toEqual(parseTime('23:55'));
+  });
+
   it('shall return expected production from mocked pokemon', () => {
     const simulator = new TeamSimulator({ settings: mockSettings, members: mockMembers, iterations: 1 });
 

nerolis-lab/nerolis-lab

backend/src/services/simulation-service/team-simulator/cooking-state/cooking-state.test.ts
@@ -1,37 +1,46 @@
 import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
 import { defaultUserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
 import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
+import type { PreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { mocks } from '@src/vitest/index.js';
-import type { MealTimes } from 'sleepapi-common';
 import {
   dessert,
   emptyIngredientInventoryFloat,
   ingredient,
   ingredientSetToFloatFlat,
   MAX_POT_SIZE,
-  parseTime
+  parseTime,
+  recipeLevelBonus
 } from 'sleepapi-common';
 import { describe, expect, it } from 'vitest';
 
+function noCritRandom(): PreGeneratedRandom {
+  const rng = (() => 0.99) as PreGeneratedRandom;
+  rng.getUint8 = () => 255;
+  rng.getIndex = () => 0;
+  rng.randomElement = <T>(array: T[]) => array[0];
+  return rng;
+}
+
 describe('CookingState', () => {
-  it('shall include provided meal times in results', () => {
+  it('shall include average cooked meal times in results', () => {
     const cookingState = new CookingState(
       mocks.teamSettings({ camp: true }),
       defaultUserRecipes(),
       createPreGeneratedRandom()
     );
 
-    const mealTimes: MealTimes = {
-      breakfast: parseTime('08:00'),
-      lunch: parseTime('14:00'),
-      dinner: parseTime('20:00')
-    };
-
-    cookingState.setMealTimes(mealTimes);
+    cookingState.recordMealCookTime('breakfast', 120);
+    cookingState.recordMealCookTime('lunch', 480);
+    cookingState.recordMealCookTime('dinner', 840);
 
     const result = cookingState.results(1);
 
-    expect(result.mealTimes).toEqual(mealTimes);
+    expect(result.mealTimes).toEqual({
+      breakfast: parseTime('08:00'),
+      lunch: parseTime('14:00'),
+      dinner: parseTime('20:00')
+    });
   });
 
   it('shall cook the best recipe for which it has ingredients', () => {
@@ -173,5 +182,77 @@ describe('CookingState', () => {
     cookingState.startNewWeek();
 
     expect(cookingState['currentDessertStockpile']).toEqual(initialStockpile);
+  });
+
+  it('shall cook a planned recipe early and add only unreserved filler value after recipe strength', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettings({
+        potSize: 20,
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'recipe', recipe: dessert.WARM_MOOMOO_MILK.name },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addIngredients(
+      ingredientSetToFloatFlat([
+        ...dessert.WARM_MOOMOO_MILK.ingredients,
+        { amount: 5, ingredient: ingredient.SLOWPOKE_TAIL }
+      ])
+    );
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: false, sunday: false })).toBe(true);
+
+    const result = cookingState.results(1);
+    expect(result.dessert.weeklyStrength).toBe(
+      dessert.WARM_MOOMOO_MILK.value * recipeLevelBonus[60] + 5 * ingredient.SLOWPOKE_TAIL.value
+    );
+    expect(result.dessert.cookedRecipes[0].averageFillerValue).toBe(5 * ingredient.SLOWPOKE_TAIL.value);
+    expect(result.dessert.cookedRecipes[0].isPlannedRecipe).toBe(true);
+  });
+
+  it('shall skip None meals without consuming a pot-size bonus', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettings({
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'none' },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addPotSize(10);
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: true, sunday: false })).toBe(false);
+    expect(cookingState['bonusPotSize']).toBe(10);
+  });
+
+  it('shall exclude ingredients reserved for planned recipes from deadline fallback recipes', () => {
+    const cookingState = new CookingState(
+      mocks.teamSettings({
+        recipeType: 'dessert',
+        mealPlan: {
+          breakfast: { kind: 'recipe', recipe: dessert.CRAFT_SODA_POP.name },
+          lunch: { kind: 'best' },
+          dinner: { kind: 'best' }
+        }
+      }),
+      defaultUserRecipes(),
+      noCritRandom()
+    );
+    cookingState.addIngredients(ingredientSetToFloatFlat(dessert.FANCY_APPLE_JUICE.ingredients));
+
+    expect(cookingState.cookPlannedMeal({ meal: 'breakfast', finalAttempt: true, sunday: false })).toBe(true);
+    const cookedRecipe = cookingState.results(1).dessert.cookedRecipes[0];
+    expect(cookedRecipe.recipe.name).toBe(dessert.FANCY_APPLE_JUICE.name);
+    expect(cookedRecipe.averageFillerValue).toBe(0);
+    expect(cookedRecipe.isPlannedRecipe).toBe(false);
   });
 });

backend/src/services/simulation-service/team-simulator/team-simulator-utils.ts
@@ -1,8 +1,8 @@
 import { calculateHelpSpeedBeforeEnergy } from '@src/services/calculator/help/help-calculator.js';
 import type { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
-import { getDefaultMealTimes } from '@src/utils/meal-utils/meal-utils.js';
+import { getDefaultMealTimes, getMealWindows } from '@src/utils/meal-utils/meal-utils.js';
 import { TimeUtils } from '@src/utils/time-utils/time-utils.js';
-import type { FunctionalEvent, ProduceFlat, TeamMember, TeamSettings, TimePeriod } from 'sleepapi-common';
+import type { FunctionalEvent, MealSlot, ProduceFlat, TeamMember, TeamSettings, TimePeriod } from 'sleepapi-common';
 import {
   berrySetToFlat,
   calculateAveragePokemonIngredientSet,
@@ -15,6 +15,7 @@ class TeamSimulatorUtilsImpl {
   public setupSimulationTimes(params: { settings: TeamSettings; cookingState?: CookingState }): {
     nightStartMinutes: number;
     mealTimeMinutesSinceStart: number[];
+    mealWindowsMinutesSinceStart: { meal: MealSlot; start: number; end: number }[];
   } {
     const { settings, cookingState } = params;
 
@@ -26,6 +27,7 @@ class TeamSimulatorUtilsImpl {
     const nightStartMinutes = TimeUtils.timeToMinutesSinceStart(settings.bedtime, settings.wakeup);
 
     const mealTimes = getDefaultMealTimes(dayPeriod);
+    const mealWindows = getMealWindows(dayPeriod);
     cookingState?.setMealTimes(mealTimes.meals);
     const mealTimeMinutesSinceStart = mealTimes.sorted.map((time) =>
       TimeUtils.timeToMinutesSinceStart(time, dayPeriod.start)
@@ -33,7 +35,12 @@ class TeamSimulatorUtilsImpl {
 
     return {
       nightStartMinutes,
-      mealTimeMinutesSinceStart
+      mealTimeMinutesSinceStart,
+      mealWindowsMinutesSinceStart: mealWindows.map(({ meal, start, end }) => ({
+        meal,
+        start: TimeUtils.timeToMinutesSinceStart(start, dayPeriod.start),
+        end: TimeUtils.timeToMinutesSinceStart(end, dayPeriod.start)
+      }))
     };
   }
 

backend/src/services/simulation-service/team-simulator/team-simulator.test.ts
@@ -4,7 +4,10 @@ import type {
   SkillActivation,
   UnitActivation
 } from '@src/services/simulation-service/team-simulator/skill-state/skill-state-types.js';
+import { CookingState } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-state.js';
+import { defaultUserRecipes } from '@src/services/simulation-service/team-simulator/cooking-state/cooking-utils.js';
 import { TeamSimulator } from '@src/services/simulation-service/team-simulator/team-simulator.js';
+import { createPreGeneratedRandom } from '@src/utils/random-utils/pre-generated-random.js';
 import { mocks } from '@src/vitest/index.js';
 import type { Berry, PokemonSpecialty, PokemonWithIngredients, TeamMember, TeamSettings } from 'sleepapi-common';
 import {
@@ -63,6 +66,23 @@ const mockMembers: TeamMember[] = [
 ];
 
 describe('TeamSimulator', () => {
+  it('shall cook a Best Recipe meal on the final simulation tick before bedtime', () => {
+    const settings = mocks.teamSettings({
+      bedtime: parseTime('23:59'),
+      mealPlan: {
+        breakfast: { kind: 'none' },
+        lunch: { kind: 'none' },
+        dinner: { kind: 'best' }
+      }
+    });
+    const cookingState = new CookingState(settings, defaultUserRecipes(), createPreGeneratedRandom());
+    const simulator = new TeamSimulator({ settings, members: [], cookingState, iterations: 1 });
+
+    simulator.simulate();
+
+    expect(simulator.results().cooking?.mealTimes.dinner).toEqual(parseTime('23:55'));
+  });
+
   it('shall return expected production from mocked pokemon', () => {
     const simulator = new TeamSimulator({ settings: mockSettings, members: mockMembers, iterations: 1 });
 